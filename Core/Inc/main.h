037aba508ab6f31fde07c3c998ffc0044de12e35
Mladen <abdiel1207@gmail.com>
2021-11-14 14:50:19 +0100

    Initial creation of ioc file.

---
+++ Core/Inc/main.h
@@ -0,0 +1,203 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_AMP_DIRECT_Pin GPIO_PIN_2
+#define LED_AMP_DIRECT_GPIO_Port GPIOE
+#define LEDS_PWM_Pin GPIO_PIN_3
+#define LEDS_PWM_GPIO_Port GPIOE
+#define ENCODER_A_Pin GPIO_PIN_4
+#define ENCODER_A_GPIO_Port GPIOE
+#define ENCODER_B_Pin GPIO_PIN_5
+#define ENCODER_B_GPIO_Port GPIOE
+#define INPUT_DAC_Pin GPIO_PIN_6
+#define INPUT_DAC_GPIO_Port GPIOE
+#define INPUT_2_Pin GPIO_PIN_13
+#define INPUT_2_GPIO_Port GPIOC
+#define DAC_ENABLE_Pin GPIO_PIN_14
+#define DAC_ENABLE_GPIO_Port GPIOC
+#define INPUT_1_Pin GPIO_PIN_15
+#define INPUT_1_GPIO_Port GPIOC
+#define DC_PROTECT_Pin GPIO_PIN_9
+#define DC_PROTECT_GPIO_Port GPIOH
+#define INPUT_0_Pin GPIO_PIN_10
+#define INPUT_0_GPIO_Port GPIOH
+#define SPEAKERS_ENABLE_Pin GPIO_PIN_0
+#define SPEAKERS_ENABLE_GPIO_Port GPIOH
+#define V_PROTECT_Pin GPIO_PIN_1
+#define V_PROTECT_GPIO_Port GPIOH
+#define BUTTONS_2_Pin GPIO_PIN_0
+#define BUTTONS_2_GPIO_Port GPIOC
+#define LED_VOLUME_Pin GPIO_PIN_1
+#define LED_VOLUME_GPIO_Port GPIOC
+#define LED_RGB_0_Pin GPIO_PIN_2
+#define LED_RGB_0_GPIO_Port GPIOC
+#define LED_RGB_1_Pin GPIO_PIN_3
+#define LED_RGB_1_GPIO_Port GPIOC
+#define ADC_VOLUME_0_Pin GPIO_PIN_0
+#define ADC_VOLUME_0_GPIO_Port GPIOA
+#define ADC_VOLUME_1_Pin GPIO_PIN_1
+#define ADC_VOLUME_1_GPIO_Port GPIOA
+#define LED_RGB_2_Pin GPIO_PIN_2
+#define LED_RGB_2_GPIO_Port GPIOA
+#define LEDS_PWM_RGB_Pin GPIO_PIN_3
+#define LEDS_PWM_RGB_GPIO_Port GPIOA
+#define ADC_BALANCE_Pin GPIO_PIN_4
+#define ADC_BALANCE_GPIO_Port GPIOA
+#define ADC_TREBLE_Pin GPIO_PIN_5
+#define ADC_TREBLE_GPIO_Port GPIOA
+#define ADC_BASS_Pin GPIO_PIN_6
+#define ADC_BASS_GPIO_Port GPIOA
+#define LEDS_PWM_AUX_Pin GPIO_PIN_7
+#define LEDS_PWM_AUX_GPIO_Port GPIOA
+#define LED_BALANCE_Pin GPIO_PIN_4
+#define LED_BALANCE_GPIO_Port GPIOC
+#define LED_TREBLE_Pin GPIO_PIN_5
+#define LED_TREBLE_GPIO_Port GPIOC
+#define LED_BASS_Pin GPIO_PIN_0
+#define LED_BASS_GPIO_Port GPIOB
+#define BUTTONS_1_Pin GPIO_PIN_1
+#define BUTTONS_1_GPIO_Port GPIOB
+#define LED_LOUDNESS_Pin GPIO_PIN_2
+#define LED_LOUDNESS_GPIO_Port GPIOB
+#define LED_SPEAKERS_B_Pin GPIO_PIN_8
+#define LED_SPEAKERS_B_GPIO_Port GPIOD
+#define LED_SPEAKERS_A_Pin GPIO_PIN_9
+#define LED_SPEAKERS_A_GPIO_Port GPIOD
+#define LED_DIRECT_Pin GPIO_PIN_10
+#define LED_DIRECT_GPIO_Port GPIOD
+#define IR_Pin GPIO_PIN_11
+#define IR_GPIO_Port GPIOD
+#define POWER_ENABLE_Pin GPIO_PIN_12
+#define POWER_ENABLE_GPIO_Port GPIOD
+#define GPIO_0_Pin GPIO_PIN_13
+#define GPIO_0_GPIO_Port GPIOD
+#define POWER_BUTTON_Pin GPIO_PIN_14
+#define POWER_BUTTON_GPIO_Port GPIOD
+#define LED_POWER_Pin GPIO_PIN_15
+#define LED_POWER_GPIO_Port GPIOD
+#define LED_STANDBY_Pin GPIO_PIN_6
+#define LED_STANDBY_GPIO_Port GPIOC
+#define LED_APD_Pin GPIO_PIN_7
+#define LED_APD_GPIO_Port GPIOC
+#define SEC_POWER_Pin GPIO_PIN_8
+#define SEC_POWER_GPIO_Port GPIOC
+#define BUZZER_PWM_Pin GPIO_PIN_9
+#define BUZZER_PWM_GPIO_Port GPIOC
+#define BT_ENABLE_Pin GPIO_PIN_8
+#define BT_ENABLE_GPIO_Port GPIOA
+#define BT_TX_Pin GPIO_PIN_9
+#define BT_TX_GPIO_Port GPIOA
+#define BT_RX_Pin GPIO_PIN_10
+#define BT_RX_GPIO_Port GPIOA
+#define BT_CTS_Pin GPIO_PIN_11
+#define BT_CTS_GPIO_Port GPIOA
+#define BT_RTS_Pin GPIO_PIN_12
+#define BT_RTS_GPIO_Port GPIOA
+#define LED_HEARTBEAT_Pin GPIO_PIN_15
+#define LED_HEARTBEAT_GPIO_Port GPIOA
+#define RELAY_0_Pin GPIO_PIN_10
+#define RELAY_0_GPIO_Port GPIOC
+#define RELAY_1_Pin GPIO_PIN_11
+#define RELAY_1_GPIO_Port GPIOC
+#define RELAY_2_Pin GPIO_PIN_12
+#define RELAY_2_GPIO_Port GPIOC
+#define DIR_CS_Pin GPIO_PIN_0
+#define DIR_CS_GPIO_Port GPIOD
+#define DIR_SCK_Pin GPIO_PIN_1
+#define DIR_SCK_GPIO_Port GPIOD
+#define VOL_RESET_Pin GPIO_PIN_2
+#define VOL_RESET_GPIO_Port GPIOD
+#define DIR_MISO_Pin GPIO_PIN_3
+#define DIR_MISO_GPIO_Port GPIOD
+#define DIR_MISOD4_Pin GPIO_PIN_4
+#define DIR_MISOD4_GPIO_Port GPIOD
+#define WM_CS_Pin GPIO_PIN_5
+#define WM_CS_GPIO_Port GPIOD
+#define DIR_RESET_Pin GPIO_PIN_6
+#define DIR_RESET_GPIO_Port GPIOD
+#define DIR_ERR_Pin GPIO_PIN_7
+#define DIR_ERR_GPIO_Port GPIOD
+#define DIR_NPCM_Pin GPIO_PIN_3
+#define DIR_NPCM_GPIO_Port GPIOB
+#define LED_SACD_Pin GPIO_PIN_4
+#define LED_SACD_GPIO_Port GPIOB
+#define LED_NETWORK_Pin GPIO_PIN_5
+#define LED_NETWORK_GPIO_Port GPIOB
+#define LED_OHONO_Pin GPIO_PIN_8
+#define LED_OHONO_GPIO_Port GPIOB
+#define LED_TUNER_Pin GPIO_PIN_9
+#define LED_TUNER_GPIO_Port GPIOB
+#define LED_AUX_Pin GPIO_PIN_0
+#define LED_AUX_GPIO_Port GPIOE
+#define LED_RECORDER_Pin GPIO_PIN_1
+#define LED_RECORDER_GPIO_Port GPIOE
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Core/Src/stm32l0xx_hal_msp.c
@@ -0,0 +1,563 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file         stm32l0xx_hal_msp.c
+  * @brief        This file provides code for the MSP Initialization
+  *               and de-Initialization codes.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN Define */
+
+/* USER CODE END Define */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN Macro */
+
+/* USER CODE END Macro */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* External functions --------------------------------------------------------*/
+/* USER CODE BEGIN ExternalFunctions */
+
+/* USER CODE END ExternalFunctions */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+                                        /**
+  * Initializes the Global MSP.
+  */
+void HAL_MspInit(void)
+{
+  /* USER CODE BEGIN MspInit 0 */
+
+  /* USER CODE END MspInit 0 */
+
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* System interrupt init*/
+
+  /* USER CODE BEGIN MspInit 1 */
+
+  /* USER CODE END MspInit 1 */
+}
+
+/**
+* @brief ADC MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hadc: ADC handle pointer
+* @retval None
+*/
+void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspInit 0 */
+
+  /* USER CODE END ADC1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_ADC1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**ADC GPIO Configuration
+    PC0     ------> ADC_IN10
+    PA0     ------> ADC_IN0
+    PA1     ------> ADC_IN1
+    PA4     ------> ADC_IN4
+    PA5     ------> ADC_IN5
+    PA6     ------> ADC_IN6
+    PB1     ------> ADC_IN9
+    */
+    GPIO_InitStruct.Pin = BUTTONS_2_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(BUTTONS_2_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = ADC_VOLUME_0_Pin|ADC_VOLUME_1_Pin|ADC_BALANCE_Pin|ADC_TREBLE_Pin
+                          |ADC_BASS_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = BUTTONS_1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(BUTTONS_1_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN ADC1_MspInit 1 */
+
+  /* USER CODE END ADC1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief ADC MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hadc: ADC handle pointer
+* @retval None
+*/
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
+{
+  if(hadc->Instance==ADC1)
+  {
+  /* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+  /* USER CODE END ADC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_ADC1_CLK_DISABLE();
+
+    /**ADC GPIO Configuration
+    PC0     ------> ADC_IN10
+    PA0     ------> ADC_IN0
+    PA1     ------> ADC_IN1
+    PA4     ------> ADC_IN4
+    PA5     ------> ADC_IN5
+    PA6     ------> ADC_IN6
+    PB1     ------> ADC_IN9
+    */
+    HAL_GPIO_DeInit(BUTTONS_2_GPIO_Port, BUTTONS_2_Pin);
+
+    HAL_GPIO_DeInit(GPIOA, ADC_VOLUME_0_Pin|ADC_VOLUME_1_Pin|ADC_BALANCE_Pin|ADC_TREBLE_Pin
+                          |ADC_BASS_Pin);
+
+    HAL_GPIO_DeInit(BUTTONS_1_GPIO_Port, BUTTONS_1_Pin);
+
+  /* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+  /* USER CODE END ADC1_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief I2C MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hi2c: I2C handle pointer
+* @retval None
+*/
+void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspInit 0 */
+
+  /* USER CODE END I2C1_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C1 GPIO Configuration
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_I2C1;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+  /* USER CODE BEGIN I2C1_MspInit 1 */
+
+  /* USER CODE END I2C1_MspInit 1 */
+  }
+  else if(hi2c->Instance==I2C2)
+  {
+  /* USER CODE BEGIN I2C2_MspInit 0 */
+
+  /* USER CODE END I2C2_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C2 GPIO Configuration
+    PB13     ------> I2C2_SCL
+    PB14     ------> I2C2_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF5_I2C2;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C2_CLK_ENABLE();
+  /* USER CODE BEGIN I2C2_MspInit 1 */
+
+  /* USER CODE END I2C2_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief I2C MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hi2c: I2C handle pointer
+* @retval None
+*/
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
+{
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspDeInit 0 */
+
+  /* USER CODE END I2C1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C1_CLK_DISABLE();
+
+    /**I2C1 GPIO Configuration
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_7);
+
+  /* USER CODE BEGIN I2C1_MspDeInit 1 */
+
+  /* USER CODE END I2C1_MspDeInit 1 */
+  }
+  else if(hi2c->Instance==I2C2)
+  {
+  /* USER CODE BEGIN I2C2_MspDeInit 0 */
+
+  /* USER CODE END I2C2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C2_CLK_DISABLE();
+
+    /**I2C2 GPIO Configuration
+    PB13     ------> I2C2_SCL
+    PB14     ------> I2C2_SDA
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_13);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_14);
+
+  /* USER CODE BEGIN I2C2_MspDeInit 1 */
+
+  /* USER CODE END I2C2_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief SPI MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hspi: SPI handle pointer
+* @retval None
+*/
+void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hspi->Instance==SPI2)
+  {
+  /* USER CODE BEGIN SPI2_MspInit 0 */
+
+  /* USER CODE END SPI2_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_SPI2_CLK_ENABLE();
+
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    /**SPI2 GPIO Configuration
+    PD0     ------> SPI2_NSS
+    PD1     ------> SPI2_SCK
+    PD3     ------> SPI2_MISO
+    PD4     ------> SPI2_MOSI
+    */
+    GPIO_InitStruct.Pin = DIR_CS_Pin|DIR_SCK_Pin|DIR_MISOD4_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_SPI2;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = DIR_MISO_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF2_SPI2;
+    HAL_GPIO_Init(DIR_MISO_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SPI2_MspInit 1 */
+
+  /* USER CODE END SPI2_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief SPI MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hspi: SPI handle pointer
+* @retval None
+*/
+void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
+{
+  if(hspi->Instance==SPI2)
+  {
+  /* USER CODE BEGIN SPI2_MspDeInit 0 */
+
+  /* USER CODE END SPI2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SPI2_CLK_DISABLE();
+
+    /**SPI2 GPIO Configuration
+    PD0     ------> SPI2_NSS
+    PD1     ------> SPI2_SCK
+    PD3     ------> SPI2_MISO
+    PD4     ------> SPI2_MOSI
+    */
+    HAL_GPIO_DeInit(GPIOD, DIR_CS_Pin|DIR_SCK_Pin|DIR_MISO_Pin|DIR_MISOD4_Pin);
+
+  /* USER CODE BEGIN SPI2_MspDeInit 1 */
+
+  /* USER CODE END SPI2_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief TIM_PWM MSP Initialization
+* This function configures the hardware resources used in this example
+* @param htim_pwm: TIM_PWM handle pointer
+* @retval None
+*/
+void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_pwm)
+{
+  if(htim_pwm->Instance==TIM2)
+  {
+  /* USER CODE BEGIN TIM2_MspInit 0 */
+
+  /* USER CODE END TIM2_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM2_CLK_ENABLE();
+  /* USER CODE BEGIN TIM2_MspInit 1 */
+
+  /* USER CODE END TIM2_MspInit 1 */
+  }
+  else if(htim_pwm->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspInit 0 */
+
+  /* USER CODE END TIM3_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM3_CLK_ENABLE();
+  /* USER CODE BEGIN TIM3_MspInit 1 */
+
+  /* USER CODE END TIM3_MspInit 1 */
+  }
+
+}
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(htim->Instance==TIM2)
+  {
+  /* USER CODE BEGIN TIM2_MspPostInit 0 */
+
+  /* USER CODE END TIM2_MspPostInit 0 */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**TIM2 GPIO Configuration
+    PA3     ------> TIM2_CH4
+    */
+    GPIO_InitStruct.Pin = LEDS_PWM_RGB_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM2;
+    HAL_GPIO_Init(LEDS_PWM_RGB_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN TIM2_MspPostInit 1 */
+
+  /* USER CODE END TIM2_MspPostInit 1 */
+  }
+  else if(htim->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspPostInit 0 */
+
+  /* USER CODE END TIM3_MspPostInit 0 */
+
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    /**TIM3 GPIO Configuration
+    PE3     ------> TIM3_CH1
+    PA7     ------> TIM3_CH2
+    PC9     ------> TIM3_CH4
+    */
+    GPIO_InitStruct.Pin = LEDS_PWM_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
+    HAL_GPIO_Init(LEDS_PWM_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LEDS_PWM_AUX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
+    HAL_GPIO_Init(LEDS_PWM_AUX_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = BUZZER_PWM_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
+    HAL_GPIO_Init(BUZZER_PWM_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN TIM3_MspPostInit 1 */
+
+  /* USER CODE END TIM3_MspPostInit 1 */
+  }
+
+}
+/**
+* @brief TIM_PWM MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param htim_pwm: TIM_PWM handle pointer
+* @retval None
+*/
+void HAL_TIM_PWM_MspDeInit(TIM_HandleTypeDef* htim_pwm)
+{
+  if(htim_pwm->Instance==TIM2)
+  {
+  /* USER CODE BEGIN TIM2_MspDeInit 0 */
+
+  /* USER CODE END TIM2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM2_CLK_DISABLE();
+  /* USER CODE BEGIN TIM2_MspDeInit 1 */
+
+  /* USER CODE END TIM2_MspDeInit 1 */
+  }
+  else if(htim_pwm->Instance==TIM3)
+  {
+  /* USER CODE BEGIN TIM3_MspDeInit 0 */
+
+  /* USER CODE END TIM3_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM3_CLK_DISABLE();
+  /* USER CODE BEGIN TIM3_MspDeInit 1 */
+
+  /* USER CODE END TIM3_MspDeInit 1 */
+  }
+
+}
+
+/**
+* @brief UART MSP Initialization
+* This function configures the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspInit(UART_HandleTypeDef* huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(huart->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
+
+  /* USER CODE END USART1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    PA11     ------> USART1_CTS
+    PA12     ------> USART1_RTS
+    */
+    GPIO_InitStruct.Pin = BT_TX_Pin|BT_RX_Pin|BT_CTS_Pin|BT_RTS_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART1_MspInit 1 */
+
+  /* USER CODE END USART1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief UART MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param huart: UART handle pointer
+* @retval None
+*/
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+  if(huart->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspDeInit 0 */
+
+  /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    PA11     ------> USART1_CTS
+    PA12     ------> USART1_RTS
+    */
+    HAL_GPIO_DeInit(GPIOA, BT_TX_Pin|BT_RX_Pin|BT_CTS_Pin|BT_RTS_Pin);
+
+  /* USER CODE BEGIN USART1_MspDeInit 1 */
+
+  /* USER CODE END USART1_MspDeInit 1 */
+  }
+
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
