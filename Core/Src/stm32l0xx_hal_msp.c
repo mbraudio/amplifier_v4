2b06ede700d3fdc571edb6666258fdf3d55dbf01
Mladen <abdiel1207@gmail.com>
2022-01-10 21:45:03 +0100

    Added flow control to uart.

--- Core/Src/stm32l0xx_hal_msp.c
+++ Core/Src/stm32l0xx_hal_msp.c
@@ -558,8 +558,10 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     /**USART1 GPIO Configuration
     PA9     ------> USART1_TX
     PA10     ------> USART1_RX
+    PA11     ------> USART1_CTS
+    PA12     ------> USART1_RTS
     */
-    GPIO_InitStruct.Pin = BT_TX_Pin|BT_RX_Pin;
+    GPIO_InitStruct.Pin = BT_TX_Pin|BT_RX_Pin|GPIO_PIN_11|GPIO_PIN_12;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -595,8 +597,10 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     /**USART1 GPIO Configuration
     PA9     ------> USART1_TX
     PA10     ------> USART1_RX
+    PA11     ------> USART1_CTS
+    PA12     ------> USART1_RTS
     */
-    HAL_GPIO_DeInit(GPIOA, BT_TX_Pin|BT_RX_Pin);
+    HAL_GPIO_DeInit(GPIOA, BT_TX_Pin|BT_RX_Pin|GPIO_PIN_11|GPIO_PIN_12);
 
     /* USART1 interrupt DeInit */
     HAL_NVIC_DisableIRQ(USART1_IRQn);
